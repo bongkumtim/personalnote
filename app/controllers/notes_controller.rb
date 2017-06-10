17f44f31c436b269812a34aebe54ce9c5aae3d78
Bong <mjbond_bong2@yahoo.com>
2017-06-10 14:09:58 +0800

    add routes and configure all note pages, tested ok

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,16 +1,26 @@
 class NotesController < ApplicationController
+	before_action :find, only: [:show, :edit, :update, :destroy]
 
 
 	def index
+		@notes =  Note.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@note = Note.new
 	end
 
 	def create
+		@note = Note.new(note_params)
+
+		if @note.save
+			redirect_to @note
+		else 
+			render 'new'
+		end
 	end 
 
 	def edit
@@ -25,8 +35,10 @@ class NotesController < ApplicationController
 	private
 
 	def find
+		@note = Note.find(params[:id])
 	end
 
 	def note_params
+		params.require(:note).permit(:title, :content)
 	end
 end
